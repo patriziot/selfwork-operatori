b40d2754cf5053324841461e480fa79771f0ca21
patriziot <52088452+patriziot@users.noreply.github.com>
2024-05-25 18:29:35 +0200

    Selfwork-operatori1

---
+++ selfwork-operatori/selfwork-operatori-1/main.js
@@ -0,0 +1,11 @@
+let a = 5;
+let b = 32;
+let c = 19;
+let d = 1;
+let e = 8;
+
+const somma = (a + b + c + d + e);
+const media = (somma / 5);
+
+alert(`La somma tra i numeri equivale a ${somma} e la media equivale a ${media}`)
+console.log(`La somma tra i numeri equivale a ${somma} e la media equivale a ${media}`)
