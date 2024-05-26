1fcfc2ccb33dfb4ccafa78c6742f699aafd232f6
patriziot <52088452+patriziot@users.noreply.github.com>
2024-05-26 07:52:38 +0200

    Selfwork operatori 2
    Note
    Ho provato a inserire un prompt per inserire l'anno di nascita per esercitarmi un po'

---
+++ selfwork-operatori-2/main.js
@@ -0,0 +1,9 @@
+let now = 2024;
+
+let born = prompt(`In che hanno sei nata/o?`);
+
+const age = (now-born);
+
+const one_hundred = (100 - age); 
+
+console.log(`Hai ${age} anni e ti mancano ${one_hundred} anni per compierne 100`);
